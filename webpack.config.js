e62287ffe15cb564fc19b9da925ed955dd7f7665
Jackie Loven <lovenj@amazon.com>
2019-05-13 20:38:40 -0700

    Added project setup with babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path'); 
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: [
+                path.resolve(__dirname, 'src'),
+            ],
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ],
+            exclude: [
+                path.resolve(__dirname, 'src/screenshots'),
+            ],
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+    }
+}
